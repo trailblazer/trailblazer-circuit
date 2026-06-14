0a274748734e4b45c47f7dad9fbbcd82cff6dab6
Nick Sutterer <apotonick@gmail.com>
2026-06-14 18:30:09 +0200

    benchmark with "faster scoping™".

--- test/benchmark/instance_method_vs_method.rb
+++ test/benchmark/instance_method_vs_method.rb
@@ -41,19 +41,43 @@ my_node_with_instance_method_and_scoped = Trailblazer::Circuit::Node::Scoped[nil
 # we can also pass the {:exec_context} in the call, just to see how that performs.
 my_node_with_instance_method_and_not_scoped = Trailblazer::Circuit::Node[nil, my_input_pipe_with_instance_but_no_scope, Trailblazer::Circuit::Processor]
 
+
+#
+# a new approach, check if we can have fast(er)™ scoping for something like VM::Filter, that doesn't really need a lot of variables
+#
+
+class MyFasterNode < Trailblazer::Circuit::Node::Scoped
+  def scope(outer_ctx, flow_options, outer_signal, **)
+          # context_implementation.scope(outer_ctx, copy_from_outer_ctx, merge_to_lib_ctx) # FIXME: {copy_from_outer_ctx} and {merge_to_lib_ctx} are attrs.
+    outer_ctx.merge(merge_to_lib_ctx) # what if we know the outer_ctx gets disposed of anyway?
+  end
+
+  # @private
+  def unscope(lib_ctx, outer_ctx, signal, outer_signal, **)
+    # raise lib_ctx.inspect
+    return {aggregate: 1}, outer_signal # this would be aggregate
+  end
+end
+
+my_node_with_instance_method_and_faster_scoped = MyFasterNode[nil, my_input_pipe_with_instance_but_no_scope,
+  Trailblazer::Circuit::Processor,
+  merge_to_lib_ctx: {exec_context: my_exec_context}
+]
+
 # lib_ctx, flow_options = Benchmark.run_node(my_node_with_merge, lib_ctx: {}, flow_options: {application_ctx: {seq: []}})
 # lib_ctx, flow_options = Benchmark.run_node(my_node_with_method_refs, lib_ctx: {}, flow_options: {application_ctx: {seq: []}})
 # lib_ctx, flow_options = Benchmark.run_node(my_node_with_instance_method_and_scoped, lib_ctx: {}, flow_options: {application_ctx: {seq: []}})
 # lib_ctx, flow_options = Benchmark.run_node(my_node_with_instance_method_and_not_scoped, lib_ctx: {exec_context: my_exec_context}, flow_options: {application_ctx: {seq: []}})
+# lib_ctx, flow_options = Benchmark.run_node(my_node_with_instance_method_and_faster_scoped, lib_ctx: {}, flow_options: {application_ctx: {seq: []}})
 #  raise flow_options.inspect
 
 
-
 # Comparison:
-#           not scoped:   144965.7 i/s
-#          method refs:   142825.2 i/s - 1.01x  slower
-#               scoped:   125197.7 i/s - 1.16x  slower
-#       merge per step:    84383.9 i/s - 1.72x  slower
+#           not scoped:   145257.5 i/s
+#          method refs:   140706.1 i/s - 1.03x  slower
+#        faster scoped:   133354.9 i/s - 1.09x  slower
+#               scoped:   126091.3 i/s - 1.15x  slower
+#       merge per step:    83178.6 i/s - 1.75x  slower
 
 
 Benchmark.ips do |x|
@@ -71,6 +95,10 @@ Benchmark.ips do |x|
 
   x.report("not scoped") {
     Benchmark.run_node(my_node_with_instance_method_and_not_scoped, lib_ctx: {exec_context: my_exec_context}, flow_options: {application_ctx: {seq: []}})
+  }
+
+  x.report("faster scoped") {
+    Benchmark.run_node(my_node_with_instance_method_and_faster_scoped, lib_ctx: {}, flow_options: {application_ctx: {seq: []}})
   }
 
   x.compare!
