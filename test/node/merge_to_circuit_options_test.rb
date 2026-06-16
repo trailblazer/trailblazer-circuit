c4d34264223610af05fb47c2863d13dbf161a8d9
Nick Sutterer <apotonick@gmail.com>
2026-06-16 19:00:24 +0200

    test the flow of circuit_options: they're passed down, and can't be altered anywhere, and can be locally overridden per node.

--- test/node/merge_to_circuit_options_test.rb
+++ test/node/merge_to_circuit_options_test.rb
@@ -1,16 +1,15 @@
 require "test_helper"
 
 class MergeToCircuitOptions_UnitTest < Minitest::Spec
-  it "what" do
+  it "merges {:exec_context} into circuit_options" do
     # DISCUSS: currently, we can only be merge_to_circuit_options or merge_to_lib_ctx.
-
 
     my_exec_context = T.def_tasks(:a, success_signal: Right)
 
     my_node = Trailblazer::Circuit::Node::MergeToCircuitOptions[
       nil,
       :a,
-      Trailblazer::Circuit::Task::Adapter::LibInterface::InstanceMethod_CircuitOptions,
+      Trailblazer::Circuit::Task::Adapter::LibInterface::InstanceMethod,
       {exec_context: my_exec_context}
     ]
 
@@ -19,5 +18,38 @@ class MergeToCircuitOptions_UnitTest < Minitest::Spec
     assert_equal lib_ctx, {}
     assert_equal flow_options, {application_ctx: {seq: [:a]}}
     assert_equal signal, Right
+  end
+end
+
+class MergeToCircuitOptions_IntegrationTest < Minitest::Spec
+  it "circuit_options per branch cannot be altered, they're passed down but can be locally overridden" do
+    my_exec_context_ab = T.def_tasks(:a, :b, :d, success_signal: Right)
+    my_exec_context_c = T.def_tasks(:c, success_signal: Right)
+    my_exec_context_z = T.def_tasks(:z, success_signal: Right)
+    my_exec_context_xyuv = T.def_tasks(:x, :y, :u, :v, success_signal: Right)
+
+    my_pipe_3 = Trailblazer::Circuit::Builder.Pipeline(
+      [:c, :c, lib_interface::InstanceMethod, exec_context: my_exec_context_c],
+      [:v, :v, lib_interface::InstanceMethod],
+    )
+
+    my_pipe_2 = Trailblazer::Circuit::Builder.Pipeline(
+      [:x, :x, lib_interface::InstanceMethod],
+      [:z, :z, lib_interface::InstanceMethod, exec_context: my_exec_context_z],
+      [:y, :y, lib_interface::InstanceMethod],
+      [:v, my_pipe_3, Trailblazer::Circuit::Processor],
+      [:u, :u, lib_interface::InstanceMethod],
+    )
+
+    my_pipe_1 = Trailblazer::Circuit::Builder.Pipeline(
+      [:a, :a, lib_interface::InstanceMethod],
+      [:z, my_pipe_2, Trailblazer::Circuit::Processor, exec_context: my_exec_context_xyuv],
+      [:b, :b, lib_interface::InstanceMethod],
+    )
+
+    assert_run my_pipe_1,
+      seq: [:a, :x, :z, :y, :c, :v, :u, :b],
+      circuit_options: {exec_context: my_exec_context_ab},
+      terminus: Right
   end
 end
