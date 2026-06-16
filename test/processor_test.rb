74e46230f2559a72343e3f0cd7616d1d8c91b6a2
Nick Sutterer <apotonick@gmail.com>
2026-06-16 17:45:07 +0200

    clean up processor_test.

--- test/processor_test.rb
+++ test/processor_test.rb
@@ -4,15 +4,15 @@ class ProcessorTest < Minitest::Spec
   # DISCUSS: we can also "be" an Adapter directly, should we spare this?
   # DISCUSS: this interface could be officially supported in Adapter, but i'm not sure
   # anyone needs it, we'll see.
-  class CircuitInterface
+  class MyCircuitInterface
     def self.call(task, lib_ctx, flow_options, signal, **circuit_options)
       task.(lib_ctx, flow_options, signal, **circuit_options)
     end
   end
 
-  it "we can see {:node} in the {circuit_options} and hence, a CircuitInterface task, has access to data stored there" do
+  it "we can see {:node} in the {circuit_options} and hence, a MyCircuitInterface task, has access to data stored there" do
     my_task_with_circuit_interface = Struct.new(:my_id) do
-      def call(lib_ctx, flow_options, signal, node:, **circuit_options) # CircuitInterface.
+      def call(lib_ctx, flow_options, signal, node:, **circuit_options) # MyCircuitInterface.
         flow_options[:application_ctx][:seq] << [my_id, node]
 
         return lib_ctx, flow_options, signal
@@ -20,17 +20,15 @@ class ProcessorTest < Minitest::Spec
     end
 
     create_circuit = Pipeline(
-      [:model, my_task_with_circuit_interface.new(:model), CircuitInterface],
+      [:model, my_task_with_circuit_interface.new(:model), MyCircuitInterface],
     )
 
     create_tw = Pipeline(
-      [:input, my_task_with_circuit_interface.new(:input), CircuitInterface],
+      [:input, my_task_with_circuit_interface.new(:input), MyCircuitInterface],
       [:call_task, create_circuit, Trailblazer::Circuit::Processor],
     )
 
-    canonical_node = Trailblazer::Circuit::Node[:Create, create_tw, Trailblazer::Circuit::Processor]
-
-    assert_run canonical_node, node: true, seq: [
+    assert_run create_tw, seq: [
       [:input, create_tw.nodes[:input]],
       [:model, create_circuit.nodes[:model]],
     ]
@@ -38,5 +36,9 @@ class ProcessorTest < Minitest::Spec
 
   it "{:start_tuple} is not passed to the nodes" do
     raise ":start_tuple test!!!"
+  end
+
+  it "we don't pass {:start_tuple} to the child nodes of the current circuit" do
+
   end
 end
