1758350d4217d1e0eb708118a07d52a7c24ddb9d
Nick Sutterer <apotonick@gmail.com>
2026-06-23 08:54:54 +0200

    Revert "in StepInterface, the target_ctx is no longer passed via lib_ctx, but as the signal." that wasn't the smartest idea, i wanted to save the Step logic from having to create a Node::Scoped, but now we have the problem that in i/o pipes, with sometimes half a dozen Steps, we don't want the :target_ctx to be set and changed. Also, having a required kwarg is much easier to debug.
    This reverts commit 720cb223abfa20e85012e6c3c4e63c46ff9873ce.

--- lib/trailblazer/circuit/task/adapter.rb
+++ lib/trailblazer/circuit/task/adapter.rb
@@ -17,8 +17,9 @@
 
         # The step interface is only used on the application level.
         class StepInterface
-          # NOTE: the target_ctx must be handed in via the signal (value-on-signal).
-          def self.call(task, lib_ctx, flow_options, target_ctx, **circuit_options)
+          def self.call(task, lib_ctx, flow_options, signal, **circuit_options)
+            target_ctx = lib_ctx.fetch(:target_ctx) # TODO: introduce second kwargs method that calls {#run_step}.
+
             result = run_step(task, target_ctx, **circuit_options)
 
             return lib_ctx, flow_options, result # value-on-signal
